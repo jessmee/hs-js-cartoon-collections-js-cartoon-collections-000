106774f7d0a248cca3a0146d99ca4aba6ae61459
Jessica Yoo <jessmee1234@gmail.com>
2018-06-20 21:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function dwarfRollCall(dwarves) {
+  var dwarves = [];
+
+    for (var i = 0; i < dwarves.length; i++) {
+      dwarves.push(i + 1 + ". " + dwarves[i]);
+    }
+  return dwarves.join (", ");
 }
+
+
+
 
 function summonCaptainPlanet(planeteerCalls){
 }
